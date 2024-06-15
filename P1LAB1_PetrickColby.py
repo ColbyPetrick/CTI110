643cad2ea4aa47781eea80063f732d558069f30a
ColbyPetrick <petrickc6752@student.faytechcc.edu>
2024-06-15 14:27:03 -0400

    Add files via upload

---
+++ P1LAB1_PetrickColby.py
@@ -0,0 +1,17 @@
+# Colby Petrick
+# June 15th 2024
+# P1LAB1
+# Simple python script showcasing proper syntax w/ variables, input and print function
+#Using firstname and lastname as variables - They are descriptive of the info being gathered.
+print('Enter your First Name:', end=' ') #Using end function to keep input on same line
+firstname = input()
+print('Enter your Last Name:', end=' ')
+lastname = input()
+
+print('Hello,', firstname, lastname + '!', 'Welcome to CTI-110')
+
+'''
+Concatenated the Lastname with ! to get the same effect as image.
+This was trialed 7 times before no issues where noticed in output - another 3 times with different inputs
+just to be safe
+'''
